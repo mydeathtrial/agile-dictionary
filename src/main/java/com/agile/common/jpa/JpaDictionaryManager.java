06fdd53811292f1e9f0d81d9d00e81fd962f3991
mydeathtrial@163.com <mydeathtrial@163.com>
2020-08-04 11:01:50 +0800

    feat(agile-jpa集成): 增加了agile-jpa中的数据扩展接口的实现类，为其提供自动字典翻译

--- src/main/java/com/agile/common/jpa/JpaDictionaryManager.java
+++ src/main/java/com/agile/common/jpa/JpaDictionaryManager.java
@@ -1,16 +1,16 @@
 package com.agile.common.jpa;
 
-import com.agile.common.dictionary.DictionaryManager;
+import com.agile.common.dictionary.DataExtendManager;
 import com.agile.common.util.DictionaryUtil;
 
 /**
  * @author 佟盟
  * 日期 2020/8/4 9:42
- * 描述 TODO
+ * 描述 为JPA组件提供字典翻译扩展
  * @version 1.0
  * @since 1.0
  */
-public class JpaDictionaryManager implements DictionaryManager {
+public class JpaDictionaryManager implements DataExtendManager {
     @Override
     public void cover(Object o) {
         DictionaryUtil.cover(o);
